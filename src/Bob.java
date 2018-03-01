ba7749e8a60109b61b5aec31ada01505b22393d9
Bettina Trejo <bettina.trejo12@gmail.com>
2018-03-01 08:45:41 -0600

    added if else conditional for responses of input

--- src/Bob.java
+++ src/Bob.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Bob {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ask Bob: ");
+        String response = sc.nextLine();
+
+
+        if (response.endsWith("?")) {
+            System.out.println("Sure");
+        } else if (response.trim().equals("")) {
+            System.out.println("Fine. Be that way!");
+        } else if (response.endsWith("!") || response.toUpperCase().equals(response)) {
+            System.out.println("Whoa, chill out!");
+        } else {
+            System.out.println("Whatever");
+        }
+
+
+
+
+    }
 }
