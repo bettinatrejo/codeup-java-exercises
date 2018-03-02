8474f7fcb0c030022f1b7732c20395c15548d9ed
Bettina Trejo <bettina.trejo12@gmail.com>
2018-03-02 13:41:06 -0600

    OOP exercise

--- src/BurgerTools.java
+++ src/BurgerTools.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
-
 public class BurgerTools {
+
+    public static String mostPopularTopping;
+    public static int averageDaysBeforeExpiration;
+    public static int temperatureWhenCooked;
+
+    public static void grill() {
+        System.out.println("Grilling Burger....");
+    }
+
+
 }
