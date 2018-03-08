72ac313a4f6f914f56ba78f79611664ad31ccea7
Bettina Trejo <bettina.trejo12@gmail.com>
2018-03-08 10:21:30 -0600

    added constructor and overrode the setlength/width and getperimeter/area

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,20 +1,49 @@
 package shapes;
 
-class Square extends Rectangle{
-    private int side;
-    public Square(int side) {
-        super(side,side);
-        this.side = side;
-    }
+class Square extends Quadrilateral{
 
-//    overriding rectangle methods
-    @Override
-    public int getArea() {
-        return side*side;
+    public Square(int width, int length) {
+        super(width, length);
     }
 
     @Override
-    public int getPerimeter() {
-        return 4*side;
+    public void setWidth(int width) {
+
     }
+
+    @Override
+    public void setLength(int length) {
+
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 4*length;
+    }
+
+    @Override
+    public double getArea() {
+        return length*length;
+    }
+
+
+
+
+
+//    private int side;
+//    public Square(int side) {
+//        super(side,side);
+//        this.side = side;
+//    }
+//
+////    overriding rectangle methods
+//    @Override
+//    public int getArea() {
+//        return side*side;
+//    }
+//
+//    @Override
+//    public int getPerimeter() {
+//        return 4*side;
+//    }
 }
