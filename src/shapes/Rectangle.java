9d7fae32658b4d93cf4fba37cdf8678153d614d3
Bettina Trejo <bettina.trejo12@gmail.com>
2018-03-08 10:18:35 -0600

    added constructor and completed override of methods

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,38 +1,57 @@
 package shapes;
 
-public class Rectangle {
-
-
-//    protected properties
-   protected int length;
-   protected int width;
-
-
-//   constructor
-    public Rectangle(int length, int width) {
-        this.length = length;
-        this.width = width;
+public class Rectangle extends Quadrilateral implements Measurable {
+    public Rectangle(int width, int length) {
+        super(width, length);
     }
 
+    @Override
+    public void setWidth(int width) {
 
-//    area method
-    public int getArea() {
-        return length * width;
     }
 
-//    perimeter method
-    public int getPerimeter() {
+    @Override
+    public void setLength(int length) {
+
+    }
+
+    @Override
+    public double getPerimeter() {
         return (2 * length) + (2 * width);
     }
+
+    @Override
+    public double getArea() {
+        return length*width;
+    }
+
+
+
+//
+////    protected properties
+//   protected int length;
+//   protected int width;
+//
+//
+////   constructor
+//    public Rectangle(int length, int width) {
+//        this.length = length;
+//        this.width = width;
+//    }
+//
+//
+////    area method
+//    public int getArea() {
+//        return length * width;
+//    }
+//
+////    perimeter method
+//    public int getPerimeter() {
+//        return (2 * length) + (2 * width);
+//    }
 
 
 }
 
 
 
-
-//tester
-
-//
-//Person p = new Person();
-//p.sayHello();
