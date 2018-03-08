8826cf8d3895adc7c6e79506832fdbed64e5cc54
Bettina Trejo <bettina.trejo12@gmail.com>
2018-03-08 10:20:12 -0600

    declared myShape variable and printed out area/perimeter

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -5,16 +5,26 @@ import org.w3c.dom.css.Rect;
 public class ShapesTest {
     public static void main(String[] args) {
 
-        Rectangle box1 = new Rectangle(5, 4);
-        System.out.println(box1.getArea());
-        System.out.println(box1.getPerimeter());
+//        Rectangle box1 = new Rectangle(5, 4);
+//        System.out.println(box1.getArea());
+//        System.out.println(box1.getPerimeter());
+//
+//
+//        Rectangle box2 = new Square(5);
+//        System.out.println(box2.getArea());
+//        System.out.println(box2.getPerimeter());
+//
 
 
-        Rectangle box2 = new Square(5);
-        System.out.println(box2.getArea());
-        System.out.println(box2.getPerimeter());
+        Measurable myShape;
 
+        myShape = new Rectangle(10,4);
+        System.out.println(myShape.getArea());
+        System.out.println(myShape.getPerimeter());
 
+        myShape = new Square(4,4);
+        System.out.println(myShape.getArea());
+        System.out.println(myShape.getPerimeter());
 
     }
 }
