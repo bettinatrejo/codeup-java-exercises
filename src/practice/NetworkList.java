b27b0e19bad01e5a1a14430bb5d3b19268704aae
Bettina Trejo <bettina.trejo12@gmail.com>
2018-03-12 08:19:58 -0500

    initial commit

---
+++ src/practice/NetworkList.java
@@ -0,0 +1,14 @@
+package practice;
+
+import java.util.HashMap;
+
+public class NetworkList {
+//    what is ls doing here?
+    static HashMap<String, network> ls;
+
+    public static void main(String[] args) {
+//        ls=readList();
+    }
+
+
+}
